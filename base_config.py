b02843b5410f4ab36e274d52c5601dac63fb524e
mvasilei <53435274+mvasilei@users.noreply.github.com>
2022-11-02 16:53:16 +0000

    Update base_config.py

--- base_config.py
+++ base_config.py
@@ -1,33 +1,38 @@
 #! /usr/bin/env python3.6
 from jinja2 import Environment, FileSystemLoader
-import signal, sys
+from optparse import OptionParser
+import signal, sys, json
+
 
 def signal_handler(sig, frame):
      print('Exiting gracefully Ctrl-C detected...')
      sys.exit()
 
 def main():
-    #This line uses the current directory
     file_loader = FileSystemLoader('.')
-    # Load the enviroment
     env = Environment(loader=file_loader)
     template = env.get_template('base_config.txt')
 
-    device = {"MCE": "Sandrine",
-         "LOCATION": "Gergeley",
-         "SNMP_SHA": "Frieda",
-         "SNMP_AES": "Fritz",
-         "TACACS_PASSWORD": "tacacs99",
-         "LOCAL_PASSWORD": "Sirius",
-         "LOOPBACK": "10.10.10.10",
-         "MPE_HSRP_IP": "60.60.60.1",
-         "VLAN600_IP": "60.60.60.15",
-         "SUBNETMASK_LENGTH": "24"}
+    usage = 'usage: %prog options [arg]'
+    parser = OptionParser(usage)
+    parser.add_option('-f', '--file', dest='filename',
+                      help='Read data from FILENAME')
+
+    (options, args) = parser.parse_args()
+
+    if not len(sys.argv) > 1:
+        parser.print_help()
+        exit()
+
+    with open(options.filename, 'r') as f:
+        data = f.read()
+
+    device = json.loads(data)
 
     output = template.render(
          MCE = device["MCE"],
-         LOCATION =  device["LOCATION"],
-         SNMP_SHA =  device["SNMP_SHA"],
+         LOCATION = device["LOCATION"],
+         SNMP_SHA = device["SNMP_SHA"],
          SNMP_AES = device["SNMP_AES"],
          TACACS_PASSWORD = device["TACACS_PASSWORD"],
          LOCAL_PASSWORD = device["LOCAL_PASSWORD"],
